100bb2d97b6d17ce4e778e82b5448ac47583ddce
AresRebatto <aresrebatt@gmail.com>
2023-01-31 20:43:59 +0100

    Conclusione Cookie + implementazione ogni page

---
+++ cookie.js
@@ -0,0 +1,18 @@
+let popUp = document.getElementById("popup")
+
+function openPopUp()
+{
+    popUp.classList.add("openpopUp")
+}
+
+
+function closePopUp()
+{
+    popUp.classList.remove("openpopUp")
+
+}
+
+function Customize()
+{
+    popUp.classList.remove("openpopUp")
+}
--- index.js
+++
@@ -1,9 +0,0 @@
-let popUp = document.getElementById("popup")
-
-popUp.classList.add("openpopUp")
-
-function closePopUp()
-{
-    popUp.classList.remove("openpopUp")
-    
-}
